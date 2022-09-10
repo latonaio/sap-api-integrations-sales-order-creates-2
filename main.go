5abe4796b62978a9636e9fd14cf140fcdbb8be9b
Yurino Oga <yurino.o@latona.io>
2022-09-10 21:27:49 +0900

    release

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 		pc,
 		l,
 	)
-	inputSDC := fr.ReadSDC("./Inputs/SDC_Sales_Order_Header_sample.json")
+	inputSDC := fr.ReadSDC("./Inputs/SDC_Sales_Order_Item_sample.json")
 	sap_api_time_value_converter.ChangeTimeFormatToSAPFormatStruct(&inputSDC)
 	
 	accepter := getAccepter(inputSDC)
